1b4588104f477628391cf2c3a4fc7e15949cd69e
ManjeeriN <manjeerinawghare@gmail.com>
2022-04-17 08:12:12 +0530

    callback

--- asynchronous.js
+++ asynchronous.js
@@ -9,3 +9,39 @@ function showTimer(){
     let currentTime = currentDateTime.getHours() + ":" + currentDateTime.getMinutes()+ ":" + currentDateTime.getSeconds()
     p.innerText = currentTime
 }
+
+// wait for the page to load until it shows
+
+var prom = document.getElementById('prom')
+
+var url = "cars.html";
+// function of a object
+var carObject = {
+name : 'Ferrari',
+speed : '100km/hr',
+carcb :function (){
+    console.log(this.speed)
+}
+}
+carObject.carcb()
+
+
+// function to get the request from server and wait for callback to run
+function carwebsite(cb){
+    let req = new XMLHttpRequest();
+    req.open('GET',url);
+    req.onload = function(){
+        if(req.status = 200) {
+            cb(this.responseText)
+        } else{
+            cb('Error: Someting Went wrong')
+        }
+    }
+    req.send()
+}
+
+function callback(resulthtml){
+    prom.innerHTML = resulthtml
+}
+
+carwebsite(callback)

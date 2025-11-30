e49d66afbf3288a705780c175f5d24edc5166975
ManjeeriN <manjeerideveloper@gmail.com>
2025-11-30 17:38:57 +0530

    if linkedlist has cycle

--- programs/reverseSinglyLinkedList.js
+++ programs/reverseSinglyLinkedList.js
@@ -53,8 +53,33 @@ const reverseLinkedList = (head) => {
 
 }
 
+
+//something is wrong, not giving desired result
+const hasCycle = (head) => {
+
+    if(!head || !head.next) return false;
+
+    let slow = head;
+    let fast = head;
+
+    while (fast && fast.next) {
+
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if(slow === fast) {
+            return true;
+        }
+    }
+    return false;
+}
+
 let head = createLinkedList([1,2,3,4,5]);
 console.log('current List: ', printList(head)) // current List:  1 -> 2 -> 3 -> 4 -> 5
 
 let newHead = reverseLinkedList(head)
-console.log('current List: ', printList(newHead))
+console.log('reversed List: ', printList(newHead))
+
+let nhead = createLinkedList([1,2,3,4,5,3]);
+console.log('check cycle : ', hasCycle(nhead));
+

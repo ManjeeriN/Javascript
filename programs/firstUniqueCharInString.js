201c609b707d822a012649a5b4ebfafa48a05bcc
ManjeeriN <manjeerideveloper@gmail.com>
2025-11-30 23:07:14 +0530

    find first unique character in string

---
+++ programs/firstUniqueCharInString.js
@@ -0,0 +1,15 @@
+function firstUniqChar(str) {
+    for (let i = 0; i <= str.length; i++) {
+        let found = false;
+        for (let j = 0; j <= str.length; j++) {
+            if((i !== j) && (str[i] === str[j])) {
+                found = true;
+                break;
+            }
+        }
+        if(!found) return str[i];
+    }
+    return '$';
+}
+
+console.log(firstUniqChar('MadanManjeeri')); // d

917a43091d89b70510251e20ea0bf8819f66c294
ManjeeriN <manjeerideveloper@gmail.com>
2025-11-30 14:49:02 +0530

    factorial 2 methods

--- programs/factorial.js
+++ programs/factorial.js
@@ -1,1 +1,21 @@
 // iterative method
+const factorial = (num) => {
+    if(num < 0) return 'Enter a non-negative number';
+    if(num === 1) return 1;
+
+    let fact = 1;
+    for (let i = 1; i <= num; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+console.log(factorial(5));
+
+// recursive method
+
+const rFactorial = (num) => {
+    if(num < 0) return 'Enter a non-negative number';
+    if(num === 1) return 1;
+    return num * rFactorial(num-1);
+}
+console.log(rFactorial(5));

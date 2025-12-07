949c6e844aec327ab2942192c57a0ddcfa349249
ManjeeriN <manjeerideveloper@gmail.com>
2025-12-07 11:32:03 +0530

    threes sum

---
+++ programs/threesum_15.js
@@ -0,0 +1,26 @@
+const threesum = (arrp) => {
+    if(arrp && arrp.length < 3 ) return [];
+    let arr = arrp.sort();
+    console.log(arr)
+    let result = []
+    for (let i = 0; i < arr.length - 2; i++) {
+
+        let left = i+1
+        let right = arr.length - 1
+
+        while(left < right) {
+            let sum = arr[i] + arr[left] + arr[right];
+            if(sum === 0) {
+                result.push([arr[i], arr[left], arr[right]]);
+                left++;
+                right--;
+            } else if(sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+    return Array.from(new Set(result.map(JSON.stringify))).map(JSON.parse)
+}
+console.log(threesum([-1,0,1,2,-1,-4,-2,-3,3,0,4]))
